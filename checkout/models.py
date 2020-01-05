eb561609c0afc66be9a0d035c83a491e9829c93a
Ubuntu <ubuntu@ip-172-31-35-172.ec2.internal>
2020-01-05 20:39:33 +0000

    Added checkout app and implemented Stripe functionality

---
+++ checkout/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from records.models import Record
+
+""" Represents customer information for each order instance"""
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    phone_number = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=40, blank=False)
+    postcode = models.CharField(max_length=20, blank=True)
+    town_or_city = models.CharField(max_length=40, blank=False)
+    street_address1 = models.CharField(max_length=40, blank=False)
+    street_address2 = models.CharField(max_length=40, blank=False)
+    county = models.CharField(max_length=40, blank=False)
+    date = models.DateField()
+
+    """ Return a string summary of the order with ID, date and user name"""
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+
+"""Represents the ordered item, the user and the quantity """
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False) #FK to Order class (above)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE, null=False) #FK to Record instance)
+    quantity = models.IntegerField(blank=False) 
+
+    """ Return instance details as a string"""
+    def __str__(self):
+        return "{0} {1} @ {2}".format(
+            self.quantity, self.record.name, self.record.price)
---
+++ checkout/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import MakePaymentForm, OrderForm
+from .models import OrderLineItem
+from django.conf import settings
+from django.utils import timezone
+from records.models import Record
+import stripe
+
+# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET
+
+
+@login_required()  # user must be logged in to make a paymnet
+def checkout(request):
+    if request.method == "POST":
+        """user is first shown the order & payment forms"""
+        order_form = OrderForm(request.POST) 
+        payment_form = MakePaymentForm(request.POST)
+        
+        """Validate both forms then save the order form with timestamp"""
+        if order_form.is_valid() and payment_form.is_valid():
+            order = order_form.save(commit=False)
+            order.date = timezone.now()
+            order.save()
+            
+            """Get user records purchased from current cart session"""
+            cart = request.session.get('cart', {})
+            total = 0 #initialize total to zero
+            
+            """Loop through cart with ID and quantity"""
+            for id, quantity in cart.items():
+                record = get_object_or_404(Record, pk=id) #get Record using PK
+                total += quantity * record.price  #calculate quantity and total price
+                order_line_item = OrderLineItem(
+                    order=order,
+                    record=record,
+                    quantity=quantity
+                )
+                order_line_item.save() #save details of items purchased
+            
+            """Try to make a payment using Stripe API"""
+            try:
+                customer = stripe.Charge.create(
+                    amount=int(total * 100), #Stripe uses cents not euros
+                    currency="EUR",
+                    description=request.user.email,
+                    card=payment_form.cleaned_data['stripe_id'] #customer id for seller
+                )
+            except stripe.error.CardError:
+                messages.error(request, "Your card was declined!")
+            
+            if customer.paid:
+                messages.error(request, "You have successfully paid")
+                request.session['cart'] = {}
+                return redirect(reverse('records')) #return to records page
+            else:
+                messages.error(request, "Unable to take payment")
+        else:
+            print(payment_form.errors)
+            messages.error(request, "We were unable to take a payment with that card!")
+    else:
+        payment_form = MakePaymentForm() #return blank forms
+        order_form = OrderForm()
+    
+    return render(request, "checkout.html", {"order_form": order_form, "payment_form": payment_form, "publishable": settings.STRIPE_PUBLISHABLE})
