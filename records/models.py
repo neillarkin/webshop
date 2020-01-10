462c78a5a1e6ee2964ef92fb7278cbb65ad2a11c
Ubuntu <ubuntu@ip-172-31-35-172.ec2.internal>
2020-01-10 21:53:39 +0000

    Added Artists app with One-To-Many relationship with Record class

--- records/models.py
+++ records/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from artists.models import Artist
 
 """ A class/model that represents a single record.
     Each line represents columns that will be stored in the database"""
@@ -7,6 +8,7 @@ class Record(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, default='0')
 
     """ This function returns the name of a record as a string"""
     def __str__(self):
