ea99693484c697fac5b84a7b5f64f21a793a4954
Ubuntu <ubuntu@ip-172-31-35-172.ec2.internal>
2020-01-13 19:43:27 +0000

    added Search functionality for Artists and added error msg 'no results'

--- search/views.py
+++ search/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from artists.models import Artist
 from records.models import Record
 
 # Create your views here.
+
 def do_search(request):
+    
     """Get form named q. Data entered in to the form will be filtered"""
+    artists = Artist.objects.filter(name__icontains=request.GET['q'])
     records = Record.objects.filter(name__icontains=request.GET['q'])
-    """ Return template and dictionary context of records"""
-    return render(request, "records.html", {"records": records})
+    
+    
+    """ Check if objects are present then Return template and dictionary context.
+        Else return message and remain on current page
+    """
+    if artists:
+        return render(request, "artists.html", {"artists": artists})  
+    elif records:
+        return render(request, "records.html", {"records": records}) 
+    else:
+       messages.error(request, "No results found!")
+       return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
