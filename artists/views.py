0bd41ecbfeaed670d641751c6c874498bbc1a88a
Ubuntu <ubuntu@ip-172-31-35-172.ec2.internal>
2020-01-13 18:14:16 +0000

    Added Artists' Records view and navbar menu items

--- artists/views.py
+++ artists/views.py
@@ -7,7 +7,8 @@ def all_artists(request):
     artists = Artist.objects.all()
     return render(request, "artists.html", {"artists": artists})
     
-def artists_records(request, id): 
+def artists_records(request, id):
+    artist = Artist.objects.get(id=id)
     artists_records = Record.objects.filter(artist_id=id)
-    return render(request, "artists_records.html", {"artists_records": artists_records})
+    return render(request, "artists_records.html", {"artist": artist, "artists_records": artists_records})
     
