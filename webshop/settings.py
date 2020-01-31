6aa76dbfc6da2296e8354043c7db468e8835351f
Ubuntu <ubuntu@ip-172-31-35-172.ec2.internal>
2020-01-31 14:59:26 +0000

    Disalbed Debug Mode

--- webshop/settings.py
+++ webshop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['330b774ff5e9411d8e649d87b22e2280.vfs.cloud9.us-east-1.amazonaws.com', 'neils-webshop.herokuapp.com']
 
